2c8ec8280cbcc63bcb29e5d029d749c069250e9e
Binh Tran <tranthaibinh111@yahoo.com>
2019-11-14 14:24:15 +0700

    Fix bug for building the angular universal

--- src/app/modules/site/site.module.ts
+++ src/app/modules/site/site.module.ts
@@ -4,16 +4,16 @@ import { CommonModule } from '@angular/common';
 
 // ANN Shop
 // modules
+import { SharedModule } from '../../shared/shared.module';
 import { SiteRoutingModule } from './site-routing.module';
 // components
-import { DanhMucSanPhamComponent } from './pages/danh-muc-san-pham/danh-muc-san-pham.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { LienHeComponent } from './pages/lien-he/lien-he.component';
-import { HuongDanDatHangComponent } from './pages/huong-dan-dat-hang/huong-dan-dat-hang.component';
-import { ZaloXemHangComponent } from './pages/zalo-xem-hang/zalo-xem-hang.component';
 import { ChinhSachBanSiComponent } from './pages/chinh-sach-ban-si/chinh-sach-ban-si.component';
-import { ThongBaoComponent } from './pages/thong-bao/thong-bao.component';
+import { DanhMucSanPhamComponent } from './pages/danh-muc-san-pham/danh-muc-san-pham.component';
 import { GioiThieuComponent } from './pages/gioi-thieu/gioi-thieu.component';
+import { HuongDanDatHangComponent } from './pages/huong-dan-dat-hang/huong-dan-dat-hang.component';
+import { LienHeComponent } from './pages/lien-he/lien-he.component';
+import { ThongBaoComponent } from './pages/thong-bao/thong-bao.component';
+import { ZaloXemHangComponent } from './pages/zalo-xem-hang/zalo-xem-hang.component';
 
 
 @NgModule({
