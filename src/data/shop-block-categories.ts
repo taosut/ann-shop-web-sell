e1b01b212409e1ffab4362d626fc251bee482793
Phan Hoàng Anh <phanhoanganh9x@gmail.com>
2019-11-16 21:21:33 +0700

    commit

--- src/data/shop-block-categories.ts
+++ src/data/shop-block-categories.ts
@@ -1,7 +1,8 @@
 export const categories = [
-    {title: 'Quần áo nữ có sẵn', url: '/category/quan-ao-nu', description: "Quần áo nữ mới về hàng", image: 'assets/images/categories/category-15.jpg'},
-    {title: 'Quần áo nam có sẵn', url: '/category/quan-ao-nam', description: "Quần áo nam mới về", image: 'assets/images/categories/category-1.jpg'},
-    {title: 'Quần áo nam order', url: '/shop-order/nam', description: "Không có sẵn ở kho", image: 'assets/images/categories/category-nam-order.jpg'},
-    {title: 'Quần áo nữ order', url: '/shop-order/nu', description: "Không có sẵn ở kho", image: 'assets/images/categories/category-nu-order.jpg'},
+    {title: 'Hàng mới về', url: '/shop', description: "Tất cả hàng mới về", image: 'assets/images/categories/new-product.png'},
+    {title: 'Quần áo nữ', url: '/category/quan-ao-nu', description: "Quần áo nữ mới về hàng", image: 'assets/images/categories/category-15.jpg'},
+    {title: 'Quần áo nam', url: '/category/quan-ao-nam', description: "Quần áo nam mới về", image: 'assets/images/categories/category-1.jpg'},
+    {title: 'Quần áo nam order', url: '/category/quan-ao-nam/hang-order', description: "Không có sẵn ở kho", image: 'assets/images/categories/category-nam-order.jpg'},
+    {title: 'Quần áo nữ order', url: '/category/quan-ao-nu/hang-order', description: "Không có sẵn ở kho", image: 'assets/images/categories/category-nu-order.jpg'},
     {title: 'Nước hoa giá rẻ', url: '/category/nuoc-hoa', description: "Nước hoa giá rẻ", image: 'assets/images/categories/category-44.jpg'},
 ];
